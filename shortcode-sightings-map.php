c606a99aa7f5cfa196490f8fd4d6db0887a536eb
aaslun <andreas.lundgren@norran.se>
2011-09-08 23:45:31 +0200

    Sightings map now works and aggregates all sightings in database onto the map. TODO: fix so that the markers look a little better.

--- shortcode-sightings-map.php
+++ shortcode-sightings-map.php
@@ -13,6 +13,7 @@ add_shortcode('sightings-map', function($parameters)
          * @var wpdb $wpdb
          */
         global $wpdb;
+        // Fetch all sightings from database
         $sightings = $wpdb->get_col( $wpdb->prepare( "SELECT meta_value FROM $wpdb->postmeta WHERE meta_key = '".SIGHTINGS_HANDLE."'" ) );
 
         $width = ''; // map width
@@ -25,24 +26,41 @@ add_shortcode('sightings-map', function($parameters)
                      'height' => '500px',
                 ), $parameters ) );
 
-    // Map container
-    ?>
+        // Map container
+        ?>
     <div id="sightings_map" style="width:<?= $width ?>; height:<?= $height ?>;">
     </div>
 
     <script type="text/javascript">
-    jQuery(document).ready(function(){
-        var latlng = new google.maps.LatLng(65,18);
-        var myOptions = {
-            zoom: 8,
-            center: latlng,
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var map = new google.maps.Map(document.getElementById('sightings_map'),
-                myOptions);
-    });
+        jQuery(document).ready(function(){
+            var map_latlng = new google.maps.LatLng(65,13);
+            var myOptions = {
+                zoom: 4,
+                center: map_latlng,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById('sightings_map'),
+                    myOptions);
+            <?
+            if(count($sightings) > 0) {
+                foreach($sightings as $sight)
+                {
+                    $sight = unserialize($sight);
+                    ?>
+                    var latlng = new google.maps.LatLng(<?= $sight['lat'] ?>,<?= $sight['lng'] ?>);
+                    var marker = new google.maps.Marker({
+                        map:map,
+                        draggable:false,
+                        animation: google.maps.Animation.DROP,
+                        position:latlng
+                    });
+                    <?
+                }
+            }
+            ?>
+        });
     </script>
-        
+
     <?
     });
 ?>
